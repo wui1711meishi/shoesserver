766909d1beecd95b13f52a9c98f637844d394328
ukewq <1198355270@qq.com>
2018-05-22 19:09:18 +0800

    fdsf

--- routes/product.js
+++ routes/product.js
@@ -11,5 +11,24 @@ router.get('/',function (req,res) {
         res.json(data)
     })
 })
+router.post('/add',function (req,res) {
+    let name=req.body.name;
+    let ename=req.body.ename;
+    let price=req.body.price;
+    let discount=req.body.discount;
+    let description=req.body.description;
+    let category=req.body.category;
+    let fine=req.body.fine;
+    let label=String(req.body['label[]']);
+
+
+    query(`insert into shoes (name,ename,price,discount,description,category,fine,label) value ('${name}','${ename}','${price}','${discount}','${description}','${category}','${fine}','${label}')`,function (err,data) {
+        if(data.affectedRows=='1'){
+            console.log('ok')
+        }else{
+            console.log('no')
+        }
+    })
+})
 
 module.exports = router;
